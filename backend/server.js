a0b15712c35d8ccf5f527f8361633411663f19a0
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-17 09:10:02 +0100

    Changed code to make server connect when DB is connected also

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workOutRoutes = require('./routes/workouts');
 
 const app = express();
@@ -14,6 +15,15 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workOutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+
+    console.log('Connection to DB succeeded');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
