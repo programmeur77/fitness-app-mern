fee6eb4ec69011f0e60bbc828633d0912b68748f
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-17 19:50:42 +0100

    Implemented Delete and Patch Functions

--- backend/controllers/workout-controllers.js
+++ backend/controllers/workout-controllers.js
@@ -1,10 +1,11 @@
 /**
- * @file Contains functions that are going to get or store data in the DB
+ * @file Contains functions that check the data received and get it from or post it in DB
  * @author Benoît PUECH
  * 17/12/22
  */
 
 const Workout = require('../models/workout');
+const mongoose = require('mongoose');
 
 const getAllWorkouts = async (req, res) => {
   const workouts = await Workout.find({})
@@ -16,10 +17,14 @@ const getAllWorkouts = async (req, res) => {
 };
 
 const getOneWorkout = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ message: 'Not a correct ID', status: 404 });
+  }
+
   const workout = await Workout.findOne({ _id: req.params.id })
     .then((workout) => res.status(200).json({ workout, status: 200 }))
     .catch((error) =>
-      res.status(400).json({ error: error.message, status: 400 })
+      res.status(404).json({ error: error.message, status: 404 })
     );
 };
 
@@ -34,4 +39,46 @@ const createWorkout = async (req, res) => {
   }
 };
 
-module.exports = { getAllWorkouts, getOneWorkout, createWorkout };
+const updateOneWorkout = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ message: 'Not a correct ID', status: 404 });
+  }
+
+  Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { ...req.body },
+    { new: true }
+  )
+    .then((workout) =>
+      res
+        .status(200)
+        .json({ workout, message: 'Successfully Updated', status: 200 })
+    )
+    .catch((error) =>
+      res.status(400).json({ error: error.message, status: 400 })
+    );
+};
+
+const deleteOneWorkout = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ message: 'Not a correct ID', status: 404 });
+  }
+
+  const workout = await Workout.deleteOne({ _id: req.params.id })
+    .then(() =>
+      res
+        .status(200)
+        .json({ message: 'Workout Successfully Deleted', status: 200 })
+    )
+    .catch((error) =>
+      res.status(400).json({ error: error.message, status: 400 })
+    );
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createWorkout,
+  deleteOneWorkout,
+  updateOneWorkout,
+};
