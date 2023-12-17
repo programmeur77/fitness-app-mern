ace8bc63003774e6a9ba81a13009a86c4a2bf374
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-17 09:10:28 +0100

    Implement POST route

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workout');
 
 const router = express.Router();
 
@@ -10,8 +11,15 @@ router.get('/:id', (req, res) => {
   res.json({ message: 'Get one Workout' });
 });
 
-router.post('/', (req, res) => {
-  res.json({ message: 'Post a new Workout' });
+router.post('/', async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 router.delete('/:id', (req, res) => {
