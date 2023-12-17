5fc2d6087c34929594b6ee454789d2e387b12374
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-12-17 07:16:30 +0100

    Changed port number with environment variable PORT call

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000');
+app.get('/', (req, res) => {
+  res.json({ message: 'Successfully connected to server' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
